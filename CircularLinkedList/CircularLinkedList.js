a2ba49a985982d502799f86aff98ccde87ae9154
Naveen <naveen@kemuri.in>
2020-04-04 23:16:32 +0530

    add insertAtFirstCircularList method

--- CircularLinkedList/CircularLinkedList.js
+++ CircularLinkedList/CircularLinkedList.js
@@ -1,14 +1,14 @@
 // Using Node class from Linkedlist.js file
-var circularNode6 = new Node(6);
-var circularNode2 = new Node(5, circularNode6);
-var circularNode3 = new Node(4, circularNode2);
-var circularNode4 = new Node(3, circularNode3);
-var circularNode5 = new Node(2, circularNode4);
-var circularNode = new Node(1, circularNode5);
-circularNode6.next = circularNode;
+var insertAtLastNode6 = new Node(6);
+var insertAtLastNode5 = new Node(5, insertAtLastNode6);
+var insertAtLastNode4 = new Node(4, insertAtLastNode5);
+var insertAtLastNode3 = new Node(3, insertAtLastNode4);
+var insertAtLastNode2 = new Node(2, insertAtLastNode3);
+var insertAtLastNode = new Node(1, insertAtLastNode2);
+insertAtLastNode6.next = insertAtLastNode;
 
-function insertAtLastCircularlist(node, value) {
-  if (node == null) {
+function insertAtLastNodeCircularlist(node, value) {
+  if (node == null || value == null) {
     return false;
   }
   var temp = node;
@@ -22,6 +22,35 @@ function insertAtLastCircularlist(node, value) {
 }
 //Expected output 1->2->-3->4->5->6->7->1-2 so on.
 console.log(
-  "insertAtLastCircularlist",
-  insertAtLastCircularlist(circularNode, 7)
+  "insertAtLastNodeCircularlist",
+  insertAtLastNodeCircularlist(insertAtLastNode, 7)
+);
+
+var insertAtFirstNode6 = new Node(6);
+var insertAtFirstNode5 = new Node(5, insertAtFirstNode6);
+var insertAtFirstNode4 = new Node(4, insertAtFirstNode5);
+var insertAtFirstNode3 = new Node(3, insertAtFirstNode4);
+var insertAtFirstNode2 = new Node(2, insertAtFirstNode3);
+var insertAtFirstNode = new Node(1, insertAtFirstNode2);
+insertAtFirstNode6.next = insertAtFirstNode;
+
+function insertAtFirstCircularList(node, value) {
+  if (node == null || value == null) {
+    return false;
+  }
+  var temp = node;
+  var newNode = new Node(value);
+  if (node !== null || node.next !== null) {
+    while (temp.next != node) {
+      temp = temp.next;
+    }
+    newNode.next = node;
+    temp.next = newNode;
+  }
+  return newNode;
+}
+//Expected output 0->1->2->-3->4->5->6->7->0->1-2 so on.
+console.log(
+  "insertAtFirstCircularList",
+  insertAtFirstCircularList(insertAtFirstNode, 0)
 );

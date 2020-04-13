c1213f7497ac0bb98244dd529dc3a3471c46137e
Naveen <naveen@kemuri.in>
2020-04-14 02:20:10 +0530

    add bubble sort for array

---
+++ Array/Array.js
@@ -0,0 +1,16 @@
+var arrayForBubbleSort = [3, 5, 9, 1, 0, 16, 2, 2, -1, 0];
+
+function bubbleSort(arr) {
+  var lastUnsorted = arr.length;
+  for (let i = 0; i < lastUnsorted; i++) {
+    for (let j = 0; j < lastUnsorted; j++) {
+      if (arr[i] > arr[j]) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log("Bubble Sort", bubbleSort(arrayForBubbleSort));

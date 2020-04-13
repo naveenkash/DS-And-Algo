df1b4c1e21730b8c13c102f8ed60b28340634ba9
Naveen <naveen@kemuri.in>
2020-04-14 02:42:23 +0530

    add selection sort for Array method

--- Array/Array.js
+++ Array/Array.js
@@ -1,7 +1,7 @@
 var arrayForBubbleSort = [3, 5, 9, 1, 0, 16, 2, 2, -1, 0];
 
 function bubbleSort(arr) {
-  var lastUnsorted = arr.length -1;
+  var lastUnsorted = arr.length - 1;
   for (let i = 0; i <= lastUnsorted; i++) {
     for (let j = 0; j <= lastUnsorted; j++) {
       if (arr[i] < arr[j]) {
@@ -14,3 +14,24 @@ function bubbleSort(arr) {
   return arr;
 }
 console.log("Bubble Sort", bubbleSort(arrayForBubbleSort));
+
+var arrayForSelectionSort = [8, 5, 1, 0, -4, 10, 2, 5];
+
+function selectionSort(arr) {
+  var minIndex,
+    temp,
+    length = arr.length - 1;
+  for (let i = 0; i <= length; i++) {
+    minIndex = i;
+    for (let j = i + 1; j <= length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+console.log("selectionSort", selectionSort(arrayForSelectionSort));

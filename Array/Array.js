c6b06b75d5eb5d55bdc5b24d0b9ef659afa11156
Naveen <naveen@kemuri.in>
2020-04-15 00:31:37 +0530

    improve bubble sort logic

--- Array/Array.js
+++ Array/Array.js
@@ -4,10 +4,10 @@ function bubbleSort(arr) {
   var length = arr.length - 1;
   for (let i = 0; i <= length; i++) {
     for (let j = 0; j <= length; j++) {
-      if (arr[i] < arr[j]) {
-        var temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }

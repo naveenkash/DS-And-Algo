8af1fc4362cdc0dc065eb548ef6c53c023c34830
Naveen <naveen@kemuri.in>
2020-04-25 18:54:36 +0530

    add two stacks in one array method

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -18,4 +18,65 @@ function reverseStringUsingStack(str) {
   }
   return strRev;
 }
-console.log("reverseStringUsingStack = ", reverseStringUsingStack(strRevChar));
+console.log(
+  "reverseStringUsingStack = ",
+  strRevChar,
+  "= ",
+  reverseStringUsingStack(strRevChar)
+);
+
+function twoStacksInOneArray(size) {
+  var top1 = -1,
+    top2 = size;
+  var arr = [];
+  function push1(num) {
+    if (top1 < top2 - 1) {
+      top1++;
+      arr[top1] = num;
+    } else {
+      console.log("Stack Overflow");
+      return;
+    }
+  }
+  function push2(num) {
+    if (top1 < top2 - 1) {
+      top2--;
+      arr[top2] = num;
+    } else {
+      console.log("Stack Overflow");
+      return;
+    }
+  }
+  function pop1() {
+    var popValue;
+    if (top1 >= 0) {
+      popValue = arr[top1];
+      top1--;
+      return popValue;
+    } else {
+      console.log("Stack Underflow");
+      return;
+    }
+  }
+  function pop2() {
+    var popValue;
+    if (top2 < size) {
+      popValue = arr[top2];
+      top2++;
+      return popValue;
+    } else {
+      console.log("Stack Underflow");
+      return;
+    }
+  }
+  twoStacksInOneArray.push1 = push1;
+  twoStacksInOneArray.push2 = push2;
+  twoStacksInOneArray.pop1 = pop1;
+  twoStacksInOneArray.pop2 = pop2;
+}
+
+twoStacksInOneArray(10);
+// twoStacksInOneArray.push1(1);
+// twoStacksInOneArray.push2(2);
+// twoStacksInOneArray.pop1();
+// twoStacksInOneArray.pop2();

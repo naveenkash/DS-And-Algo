20541caf9ad43f583901f565569ddbd5a9c13889
Naveen <naveen@kemuri.in>
2020-05-02 20:51:40 +0530

    add Queue basic enQueue and deQueue method using array

---
+++ Queue/Queue.js
@@ -0,0 +1,33 @@
+function Queue() {
+  var queue = [],
+    front = -1,
+    rear = -1;
+  function enQueue(value) {
+    if (value == null) {
+      return;
+    }
+    if (queue.length == 0) {
+      front++;
+    }
+    queue.push(value);
+    rear++;
+  }
+  function deQueue() {
+    if (queue.length == 1) {
+      front = -1;
+    }
+    rear--;
+    console.log(queue.shift()); // return queue.shift();
+  }
+  Queue.enQueue = enQueue;
+  Queue.deQueue = deQueue;
+}
+Queue();
+// Queue.enQueue(1);
+// Queue.enQueue(2);
+// Queue.enQueue(3);
+// Queue.enQueue(4);
+// Queue.deQueue();
+// Queue.deQueue();
+// Queue.deQueue();
+// Queue.deQueue();
